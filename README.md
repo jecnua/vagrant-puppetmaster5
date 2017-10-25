f9ff57c6d3fd4a0cb7dad9d6027205b7652323e3
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-10-25 14:46:37 +0100

    Fixed version of docker for testing

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Puppet master 5 vagrant test with docker
+
+- https://hub.docker.com/r/puppet/puppetdb/tags/
+- https://hub.docker.com/r/puppet/puppetexplorer/tags/
+- https://hub.docker.com/r/puppet/puppetboard/tags/
+- https://hub.docker.com/r/puppet/puppetdb-postgres/tags/
+- https://hub.docker.com/r/puppet/puppetserver/tags/
+- https://hub.docker.com/r/puppet/puppet-agent-ubuntu/tags/
--- bootstrap.sh
+++ bootstrap.sh
@@ -22,7 +22,7 @@ docker run \
   -v /home/ubuntu/puppet/ssl:/etc/puppetlabs/puppet/ssl/ \
   -v /home/ubuntu/puppet/serverdata:/opt/puppetlabs/server/data/puppetserver/ \
   -e "PUPPETDB_SERVER_URLS=https://puppetdb:8081" \
-  puppet/puppetserver
+  puppet/puppetserver:5.1.3
 
 docker stop postgres
 docker rm postgres
@@ -33,7 +33,7 @@ docker run --net=host \
     -v /home/ubuntu/puppetdb-postgres/data:/var/lib/postgresql/data/ \
     -e "PPOSTGRES_PASSWORD=puppetdb" \
     -e "POSTGRES_USER=puppetdb" \
-    puppet/puppetdb-postgres
+    puppet/puppetdb-postgres:9.6.3
 
 # https://github.com/puppetlabs/puppet-in-docker/blob/master/puppetdb/conf.d/database.conf#L4
 # https://docs.puppet.com/puppetdb/2.3/configure.html#subname
@@ -64,7 +64,7 @@ docker run --net=host \
     --name='puppetboard' \
     -e 'PUPPETDB_HOST=localhost' \
     -p 8000:8000 \
-    puppet/puppetboard
+    puppet/puppetboard:0.2.2
 
 docker stop puppetexplorer
 docker rm puppetexplorer
@@ -73,7 +73,7 @@ docker run \
     --name='puppetexplorer' \
     --add-host puppetdb:172.16.3.2 \
     -p 80:80 \
-    puppet/puppetexplorer
+    puppet/puppetexplorer:2.0.0
 
 # docker stop puppetagent
 # docker rm puppetagent
@@ -82,4 +82,4 @@ docker run \
 #   -h `uuidgen` \
 #   --name='puppetagent' \
 #   --add-host puppet:172.16.3.2 \
-#   puppet/puppet-agent-ubuntu agent --verbose --no-daemonize --summarize
+#   puppet/puppet-agent-ubuntu:5.3.2 agent --verbose --no-daemonize --summarize
